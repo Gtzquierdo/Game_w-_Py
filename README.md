d24f84f72381a1b90bada49215603cfb7ac443c1
Giancarlo <giancarlo@pop-os.localdomain>
2021-02-14 16:36:16 -0500

    update game(window) title and background color

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Game_w-_Py
 <!-- Working on it in PopOS -->
-<!-- Nothing fancy, just an app made to practice and put the knowledge into an application -->
+<!-- Nothing fancy, just an app made to practice and put the knowledge into an application  -->
--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import pygame
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("FIrst Game!")
+
+WHITE = (255, 255, 255)
 
 def main():
     
+    run = True
+    while run:
+        for event in pygame.event.get():
+            #  "x" will quit the window
+            if event.type == pygame.QUIT:
+                run = False
+        
+        WIN.fill((WHITE))
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
